49945cf3d093ea0ccc720866a990af1a8eee5ad8
Jonathan CHAMPION <jonathan.champion@smile.fr>
2015-06-19 16:25:24 +0200

    Refactor some & remove debug & lint

--- promesse.js
+++ promesse.js
@@ -1,97 +1,75 @@
 'use strict';
 
-var counter = 0;
-
-var Promesse = function(p) {
+var Promesse = function (p) {
     this.state = 'pending';
     this.value = undefined;
-    this.reason = undefined;
     this.deferred = [];
 
-    var resolve = (function(value) {
-        this.state = 'fulfilled';
-        this.value = value;
-        this._fulfill();
-    }).bind(this);
+    var self = this, resolve = function (state, value) {
+        self.state = state;
+        self.value = value;
+        self._resolveDeferred();
+    }, reject = function (reason) {
+        resolve('rejected', reason);
+    }, frozen = false;
 
-    var reject = (function(reason) {
-        this.state = 'rejected';
-        this.reason = reason;
-        this._reject();
-    }).bind(this);
-
-    var frozen = false;
-    p((function(value) {
-        if (frozen) return;
+    p(function (value) {
+        if (frozen) {
+            return;
+        }
         frozen = true;
 
-        this._resolve({promise:this, resolve: resolve, reject: reject}, value);
-    }).bind(this), (function(reason) {
-        if (frozen) return;
+        self._resolve({promise: self, resolve: function (value) {
+            resolve('fulfilled', value);
+        }, reject: reject}, value);
+    }, function (reason) {
+        if (frozen) {
+            return;
+        }
         frozen = true;
-
         reject(reason);
-    }).bind(this));
+    });
 };
 
-Promesse.prototype.then = function(onFulfilled, onRejected) {
+Promesse.prototype.then = function (onFulfilled, onRejected) {
     var deferred = this.defer();
     deferred.onFulfilled = onFulfilled;
     deferred.onRejected = onRejected;
 
     this.deferred.push(deferred);
 
-    if ('fulfilled' === this.state) {
-        this._fulfill();
-    } else if ('rejected' === this.state) {
-        this._reject();
-    }
+    this._resolveDeferred();
 
     return deferred.promise;
 };
 
-Promesse.prototype._fulfill = function() {
-    this.deferred.forEach((function(deferred) {
-        if ('function' !== typeof deferred.onFulfilled) {
-            // TBC
-            deferred.resolve(this.value);
+Promesse.prototype._resolveDeferred = function () {
+    if ('pending' === this.state) {
+        return;
+    }
+
+    var self = this, thenMethod = 'fulfilled' === this.state ? 'onFulfilled' : 'onRejected';
+    this.deferred.forEach(function (deferred) {
+        if ('function' !== typeof deferred[thenMethod]) {
+            deferred['fulfilled' === self.state ? 'resolve' : 'reject'](self.value);
         } else {
-            setImmediate((function() {
+            setImmediate(function () {
                 try {
-                    var x = deferred.onFulfilled.call(undefined, this.value);
-                    this._resolve(deferred, x);
+                    var x = deferred[thenMethod].call(undefined, self.value);
+                    self._resolve(deferred, x);
                 } catch (e) {
                     deferred.reject(e);
                 }
-            }).bind(this));
+            });
         }
-    }).bind(this));
+    });
     this.deferred = [];
 };
 
-Promesse.prototype._reject = function() {
-    this.deferred.forEach((function(deferred) {
-        if ('function' !== typeof deferred.onRejected) {
-            deferred.reject(this.reason);
-        } else {
-            setImmediate((function() {
-                try {
-                    var x = deferred.onRejected.call(undefined, this.reason);
-                    this._resolve(deferred, x);
-                } catch (e) {
-                    deferred.reject(e);
-                }
-            }).bind(this));
-        }
-    }).bind(this));
-    this.deferred = [];
-};
-
-Promesse.prototype.defer = function() {
+Promesse.prototype.defer = function () {
     var deferred = {};
 
-    deferred.uniqid = counter++;
-    deferred.promise = new Promesse(function(resolveFct, rejectFct) {
+    deferred.promise = new Promesse(function (resolveFct, rejectFct) {
         deferred.resolve = resolveFct;
         deferred.reject = rejectFct;
     });
@@ -106,7 +84,7 @@ Promesse.prototype.defer = function() {
  * @param x
  * @private
  */
-Promesse.prototype._resolve = function(deferred, x) {
+Promesse.prototype._resolve = function (deferred, x) {
     if (x === deferred.promise) {
         // 2.3.1
         deferred.reject(new TypeError('Uh'));
@@ -123,24 +101,29 @@ Promesse.prototype._resolve = function(deferred, x) {
         // 2.3.3
         try {
             // 2.3.3.1
-            var then = x.then;
+            var then = x.then, frozen = false;
             if ('function' === typeof then) {
                 // 2.3.3.3
-                var ignore = false;
                 try {
                     then.call(x, function (y) {
                         // 2.3.3.3.1
-                        if (ignore) return;
-                        ignore = true;
+                        if (frozen) {
+                            return;
+                        }
+                        frozen = true;
                         this._resolve(deferred, y);
                     }.bind(this), function (r) {
                         // 2.3.3.3.2
-                        if (ignore) return;
-                        ignore = true;
+                        if (frozen) {
+                            return;
+                        }
+                        frozen = true;
                         deferred.reject(r);
                     }.bind(this));
                 } catch (e) {
-                    if (!ignore) throw e;
+                    if (!frozen) {
+                        throw e;
+                    }
                 }
             } else {
                 // 2.3.3.4
